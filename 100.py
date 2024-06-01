f184b2ec1c1ec08da769f08b6ca7dedb73f0d4d4
Vitor <seuemail>
2024-06-01 19:19:39 -0300

    primeiro exercicsio

---
+++ 100.py
@@ -0,0 +1,23 @@
+cache = {1: 1}
+
+def collatz_length(n):
+    if n not in cache:
+        cache[n] = collatz_length(n // 2) + 1 if n % 2 == 0 else collatz_length(3 * n + 1) + 1
+    return cache[n]
+
+def max_collatz_length(a, b):
+    m = 0
+    for num in range(a, b + 1):
+        m = max(m, collatz_length(num))
+    return m
+
+def main_function():
+    while True:
+        try:
+            a, b = map(int, input().split())
+            print(f"{a} {b} {max_collatz_length(min(a, b), max(a, b))}")
+        except EOFError:
+            break
+
+if __name__ == "__main__":
+    main_function()
